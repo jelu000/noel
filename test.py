c56c82c46c841eb688ddae1c7d0f069950e594f7
Jens Lundeqvist <jelu000@gmail.com>
2024-05-29 23:58:17 +0200

    Test!

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+import random
+
 print(f"Hello World!")
+
+a=5
+
+slump = random.randint(1, 10)
+
+print(f"slumptal = {slump}")
+
+if slump > a:
+    print(f"Slumptalet är mer än {a}")
+
+else:
+    print("Slumptal är mindre än 6")
